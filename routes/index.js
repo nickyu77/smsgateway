b3131ff8f960fef6edeb4dd1b0423f5c64036938
nickyu77 <nickyu77@gmail.com>
2018-01-24 19:36:21 -0500

    Update index.js
    Use same http client

--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,21 @@ var router = express.Router();
 var querystring = require('querystring');
 const axios = require('axios').create({});
 
-let token = 'QUM2MmYxOTdkMmQ3MzFjNDM2YzYyMjcwYzk5OWJmZGM5ZjplYmVjZmFjNzU1NWQzNDhiMWY0Mjk4OTA1MTgzMjljNw==';
 
-axios.defaults.baseURL = 'https://api.twilio.com/2010-04-01/Accounts/AC62f197d2d731c436c62270c999bfdc9f/';
-// axios.defaults.headers.common['Authorization'] = 'Basic '+ token;
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-axios.defaults.headers['authorization'] = 'Basic '+ token;
-axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
 
 var accountSid;
 var authToken;
 
 var twilio = require('twilio');
 //var client = new twilio(accountSid, authToken);
-var client;
+//var client;
 
 //Get message from Desktop
-const desktopClient = require('axios').create({});
 
-desktopClient.defaults.baseURL = 'https://dpt.theismailiusa.org/api/v2/tickets/2269/messages';
+axios.defaults.baseURL = 'https://dpt.theismailiusa.org/api/v2/tickets/2269/messages';
 
-desktopClient.defaults.headers['authorization'] = 'key 1:N7MJ53GA2TNSYS4GHDPZ88YAS';
-desktopClient.defaults.headers['Content-Type'] = 'application/json';
+axios.defaults.headers['authorization'] = 'key 1:N7MJ53GA2TNSYS4GHDPZ88YAS';
+axios.defaults.headers['Content-Type'] = 'application/json';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -37,17 +30,24 @@ router.post('/message', function(req, res, next) {
   let toMobile = req.body.to || '+18052083159';
   accountSid = req.body.account_sid;
   authToken = req.body.auth_token;
-  client = new twilio(accountSid, authToken);
+  //client = new twilio(accountSid, authToken);
   
   console.log('fromMobile:',fromMobile);
   console.log('toMobile:',toMobile);
   console.log('message:',message);
   
-  desktopClient.get()
+  axios.get()
   .then(function(response){
     console.log(response.data); // ex.: { user: 'Your User'}
     console.log(response.status); // ex.: 200
+  }).catch(e => {
+    console.log(e)
+    next();
   });
+  
+  axios.defaults.baseURL = 'https://api.twilio.com/2010-04-01/Accounts/' + accountSid + '/';
+	axios.defaults.headers['authorization'] = 'Basic '+ authToken;
+	axios.defaults.headers['Content-Type'] = 'application/x-www-form-urlencoded';
   
 
   axios.post('Messages.json',
